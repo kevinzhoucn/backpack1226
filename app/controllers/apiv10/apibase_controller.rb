a72b9bc9c3370b42647de1b17bbd5fcbd2fb8e90
zzpf <zzpf@163.com>
2015-04-12 22:08:00 +0800

    thread query

--- app/controllers/apiv10/apibase_controller.rb
+++ app/controllers/apiv10/apibase_controller.rb
@@ -5,36 +5,71 @@ class Apiv10::ApibaseController < Apiv10::ApplicationController
     device = Device.where(:device_id => device_id).first
 
     send_flag = Cmdquery.last
-    i = 0
+    i = 10
+
+    5.times {
+      cmd = Cmdquery.last
+      send_flag = cmd.send_flag
+
+      if send_flag == 'N'
+        @cmdqueries = []
+        @channels = Channel.where(:device_id => device)
+
+        @channels.each do |channel|
+          @cmdqueries << Cmdquery.where(:channel_id => channel).last
+        end
+
+        cmd.update_attributes( send_flag: 'Y')
+
+        if @cmdqueries
+          items = []
+          @cmdqueries.each do |cmd|
+            item = {}
+            item[:channel] = cmd.channel_user_id
+            item[:value] = cmd.value        
+            items << item
+          end
+
+          ret = { :result => {:code => "1", :message => "success!"}, :data => { :set => items } }
+          render json: ret.to_json
+        end
+        return
+      end
+
+      sleep(2)
+    }
+
+    ret = { :result => {:code => "0", :message => "fail!"}, :data => { } }
+    render json: ret.to_json
 
     # while send_flag != 'N' and i++ < 5
     #   send_flag = Cmdquery.last      
     #   sleep(1)
     # end
 
-    sleep(15)
+    # sleep(15)
 
-    @cmdqueries = []
-    @channels = Channel.where(:device_id => device)
+    # @cmdqueries = []
+    # @channels = Channel.where(:device_id => device)
 
-    @channels.each do |channel|
-      @cmdqueries << Cmdquery.where(:channel_id => channel).last
-    end
+    # @channels.each do |channel|
+    #   @cmdqueries << Cmdquery.where(:channel_id => channel).last
+    # end
 
-    if @cmdqueries
-      items = []
-      @cmdqueries.each do |cmd|
-        item = {}
-        item[:channel] = cmd.channel_user_id
-        item[:value] = cmd.value        
-        items << item
-      end
+    # if @cmdqueries
+    #   items = []
+    #   @cmdqueries.each do |cmd|
+    #     item = {}
+    #     item[:channel] = cmd.channel_user_id
+    #     item[:value] = cmd.value        
+    #     items << item
+    #   end
 
-      ret = { :result => {:code => "1", :message => "success!"}, :data => { :set => items } }
-      render json: ret.to_json
-    else
-      ret = { :result => {:code => "0", :message => "fail!"}, :data => { } }
-      render json: ret.to_json
-    end
+    #   ret = { :result => {:code => "1", :message => "success!"}, :data => { :set => items } }
+    #   render json: ret.to_json
+    # else
+    #   ret = { :result => {:code => "0", :message => "fail!"}, :data => { } }
+    #   render json: ret.to_json
+    # end
   end
 end
