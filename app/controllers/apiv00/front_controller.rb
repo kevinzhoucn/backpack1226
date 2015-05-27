3a1323b7d5736a08a7509f3337759c894e7cf5fd
zzpf <zzpf@163.com>
2015-05-27 19:29:56 +0800

    xxtea

--- app/controllers/apiv00/front_controller.rb
+++ app/controllers/apiv00/front_controller.rb
@@ -5,5 +5,16 @@ class Apiv00::FrontController < Apiv00::ApplicationController
 
     channel = Channel.last
     @cmd = Cmdquery.where(:channel_id => channel, :send_flag => 'N' ).last 
+
+
+    str01 = "123456789"
+    str_key = "1234567890abcdef"
+
+    # @str_to_long = XXTEA.encrypt(str01, str_key)
+    @str_to_long = XXTEA.encode_test(str01, str_key)
+
+    # @str_to_long = XXTEA.encrypt(str01, str_key).to_s
+
+    render json: @str_to_long.to_json
   end
 end
