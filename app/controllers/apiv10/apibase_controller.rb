7b35db3f2bcaef1f9db04d99c0f679206517d4af
zzpf <zzpf@163.com>
2015-06-08 18:38:46 +0800

    api

--- app/controllers/apiv10/apibase_controller.rb
+++ app/controllers/apiv10/apibase_controller.rb
@@ -81,100 +81,6 @@ class Apiv10::ApibaseController < Apiv10::ApplicationController
     render text: "result:" + ret_encrypt_str
   end
 
-  def cmdquery_01
-    device_id = params[:dev_id]
-    device = Device.where(:device_id => device_id).first
-
-    # send_flag = Cmdquery.last
-    i = 10
-
-    5.times {
-      # cmd = Cmdquery.last
-      cmd = Cmdquery.where(:device_user_id => device_id).last
-      if cmd
-        send_flag = cmd.send_flag
-
-        if send_flag == "N"
-          @cmdqueries = []
-          @channels = Channel.where(:device_id => device)
-
-          @channels.each do |channel|
-            t_cmd = Cmdquery.where(:channel_id => channel, :send_flag => 'N' ).last
-            if t_cmd
-              @cmdqueries << t_cmd
-            end
-            # @cmdqueries << Cmdquery.where(:channel_id => channel, :send_flag => 'N' ).last
-            # @cmdqueries << Cmdquery.last
-          end
-
-          if @cmdqueries.length > 0
-            #### old method
-            # items = []
-            # @cmdqueries.each do |cmd|
-            #   item = {}
-            #   item[:channel] = cmd.channel_user_id
-            #   item[:value] = cmd.value        
-            #   items << item
-
-            #   cmd.update_attributes( send_flag: 'Y')
-            # end
-
-            #### new method
-            items = ""
-            @cmdqueries.each do |cmd|
-              item = ""
-              item += cmd.channel_user_id + '-'              
-              item += cmd.value + '_'
-              items += item
-
-              cmd.update_attributes( send_flag: 'Y')
-            end
-
-            ret = { :result => "0", :data => {:set => items.chop } }
-            render json: ret.to_json
-            return
-          end
-        end
-      end
-
-      sleep(5)
-    }
-
-    # ret = { :result => {:code => "1", :message => "fail, time out!"} }
-    ret = { :result => "1" }
-    render json: ret.to_json
-
-    # while send_flag != 'N' and i++ < 5
-    #   send_flag = Cmdquery.last      
-    #   sleep(1)
-    # end
-
-    # sleep(15)
-
-    # @cmdqueries = []
-    # @channels = Channel.where(:device_id => device)
-
-    # @channels.each do |channel|
-    #   @cmdqueries << Cmdquery.where(:channel_id => channel).last
-    # end
-
-    # if @cmdqueries
-    #   items = []
-    #   @cmdqueries.each do |cmd|
-    #     item = {}
-    #     item[:channel] = cmd.channel_user_id
-    #     item[:value] = cmd.value        
-    #     items << item
-    #   end
-
-    #   ret = { :result => {:code => "0", :message => "success!"}, :data => { :set => items } }
-    #   render json: ret.to_json
-    # else
-    #   ret = { :result => {:code => "0", :message => "fail!"}, :data => { } }
-    #   render json: ret.to_json
-    # end
-  end
-
   def datetime
     data = params[:data]
     user_id = params[:user]
@@ -306,13 +212,32 @@ class Apiv10::ApibaseController < Apiv10::ApplicationController
     def set_device
       @device = Device.find(params[:id])
     end
+    
     def get_params(data)
       url_params = {}
 
-      data.split( /&/ ).inject( Hash.new { | h, k | h[k]='' } ) do | h, s |
-        k, v = s.split( /=/ )
-        h[k] << v
-        h
+      begin
+        data.split( /&/ ).inject( Hash.new { | h, k | h[k]='' } ) do | h, s |
+          k, v = s.split( /=/ )
+          h[k] << v
+          url_params = h
+        end        
+      rescue Exception => e
+        url_params
+      end      
+    end
+
+    def set_params
+      user_email = params[:user]
+      user_data = params[:data]
+      # @ret_str = "0,"
+
+      if User.where(email: @user_email).exists?
+        @user = User.where(email: user_email).first
+      end
+
+      if user_data and user_data.length > 8
+        @data_params = get_params(user_data)
       end
     end
 end
