c8ea7ac1833be03f113026d9bf7ade88a2fcc1ed
zzpf <zzpf@163.com>
2015-09-02 10:03:47 +0800

    mobile front

--- config/routes.rb
+++ config/routes.rb
@@ -76,7 +76,7 @@ Rails40Starter::Application.routes.draw do
   end
 
   namespace 'mobile' do
-    get 'front/index'
+    get '/sayhello' => '/front/index'
     get '/datetime' => 'front#datetime'
     post '/users' => 'front#create_user'
     get '/signin' => 'front#signin_user'
