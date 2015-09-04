69e7122659b34d05f94c28ce88a56558c9a12830
zzpf <zzpf@163.com>
2015-09-04 17:34:05 +0800

    xxtea

--- public/js/sdk/xxtea.js
+++ public/js/sdk/xxtea.js
@@ -1,43 +1,64 @@
-$(document).ready( function() {        
-    var str = $('#str').text();
-    var key = $('#key').text();
-    $(".encrypt-button").click(function(){        
-        $.post("/sdk/xxtea/encrypt",
-        {
-            str: str,
-            key: key
-        },
-        function(data, status){
-            // alert("Data: " + data + "\nStatus: " + status);
-            var txt="<p>" + data + "</p>";
-            $(".result").append(txt);
+var xxtea = function($){
+  var cacheElements = {
+    "str_xxtea" : $('#str').text(),
+    "str_key" : $('#key').text(),
+    "str_md5" : $('#str_md5').text(),
+    "key_md5" : $('#key_md5').text()
+  };
+  return {
+    init : function(){
+        this.bindEvents();
+    },
+    bindEvents : function(){
+        $(".encrypt-button").click(function(event){
+            event.preventDefault();
+            $.post("/sdk/xxtea/encrypt",
+            {
+                // str: cacheElements.str_xxtea,
+                // key: cacheElements.str_key
+                str: $('#str').val(),
+                key: $('#key').val()
+            },
+            function(data, status){
+                // alert("Data: " + data + "\nStatus: " + status);
+                var txt="<p>" + data + "</p>";
+                $(".result").append(txt);
+            });
         });
-    });
-    $(".decrypt-button").click(function(){        
-        $.post("/sdk/xxtea/decrypt",
-        {
-            str: str,
-            key: key
-        },
-        function(data, status){
-            // alert("Data: " + data + "\nStatus: " + status);
-            var txt="<p>" + data + "</p>";
-            $(".result").append(txt);
+        $(".decrypt-button").click(function(event){
+            event.preventDefault();
+            $.post("/sdk/xxtea/decrypt",
+            {
+                // str: cacheElements.str_xxtea,
+                // key: cacheElements.str_key
+                str: $('#str').val(),
+                key: $('#key').val()
+            },
+            function(data, status){
+                // alert("Data: " + data + "\nStatus: " + status);
+                var txt="<p>" + data + "</p>";
+                $(".result").append(txt);
+            });
         });
-    });
+        $(".md5-encrypt-button").click(function(event){
+            event.preventDefault();
+            $.post("/sdk/md5/encrypt",
+            {
+                // str: cacheElements.str_md5,
+                // key: cacheElements.key_md5
+                str: $('#str_md5').val(),
+                key: $('#key_md5').val()
+            },
+            function(data, status){
+                // alert("Data: " + data + "\nStatus: " + status);
+                var txt="<p>" + data + "</p>";
+                $(".result-md5").append(txt);
+            });
+        });    
+    }
+  }
+}(jQuery);
 
-    var str_md5 = $('#str_md5').text();
-    var key_md5 = $('#key_md5').text();
-    $(".md5-encrypt-button").click(function(){        
-        $.post("/sdk/md5/encrypt",
-        {
-            str: str_md5,
-            key: key_md5
-        },
-        function(data, status){
-            // alert("Data: " + data + "\nStatus: " + status);
-            var txt="<p>" + data + "</p>";
-            $(".result-md5").append(txt);
-        });
-    });
+$(document).ready( function() {
+    xxtea.init();
 });
