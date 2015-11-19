13f53c5530e5eee57cd1e68fff035ae696dcbff8
zzpf <zzpf@163.com>
2015-11-19 16:15:53 +0800

    modify application layout

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -1,7 +1,7 @@
 class FrontController < ApplicationController
-  before_filter :authenticate_user!, :only => [:profile, :new_device, :new_channel, :show_chart, :show_device, :edit_channel, :edit_device]
-  layout 'appprofile', :only => [:profile, :new_device, :new_channel, :show_chart, :show_device, :edit_channel, :edit_device, :show_channel_chart]
+  before_filter :authenticate_user!, :only => [:profile, :new_device, :new_channel, :show_chart, :show_device, :edit_channel, :edit_device]  
   layout :false, :only => [:index]
+  layout 'appprofile', :only => [:profile, :new_device, :new_channel, :show_chart, :show_device, :edit_channel, :edit_device, :show_channel_chart]
 
   before_action :set_device, only: [:show_channel_chart]
   before_action :set_channel, only: [:get_channel_data, :show_channel_chart]
